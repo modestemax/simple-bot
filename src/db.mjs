d27f36cc500a6fbe9beb344e48524f7404ec634d
max <modestemax@gmail.com>
2020-08-21 14:15:34 +0100

    ...

--- src/db.mjs
+++ src/db.mjs
@@ -13,6 +13,10 @@ export class Signal{
 		const percent=(((this.close-this.open)/this.open)*100)
 		return Math.trunc(percent*100)/100 
 	}
+	#max =0;
+	get max(){
+	return	this.#max= Math.max(this.percent,this.#max)
+	}
 }
 
 export const first=new Signal({})
@@ -20,5 +24,5 @@ export const first=new Signal({})
 export const findFirst=(cryptoMap)=>{
 	const sortedList=Object.values(cryptoMap).filter(a=>a.percent).sort((a,b)=>a.percent<b.percent?1:-1)
 	Object.assign(first, sortedList[0])
-	console.log(first,first.percent)
+	console.log(first,'percent=',first.percent,'max=',first.max)
 }
