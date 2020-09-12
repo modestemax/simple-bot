f45ec9d681b04af39fb8878e895f7bd9b00c3fa2
modestemax <modestemax@gmail.com>
2020-09-13 00:02:07 +0100

    update

--- src/strategies/_first_.mjs
+++ src/strategies/_first_.mjs
@@ -1,21 +1,25 @@
 import {cryptoMap} from "../db/index.mjs";
 import consola from "consola";
 
+let gotProfit
+let lossCount = 0
 
 export default {
     enter(signal) {
         const sortedByPercent = Object.values(cryptoMap).filter(a => a.percent)
             .sort((a, b) => a.percent < b.percent ? 1 : -1)
-        return (signal.symbol === sortedByPercent[0]?.symbol && signal.percent >= config.enter_trade)
+        return !gotProfit && (signal.symbol === sortedByPercent[0]?.symbol && signal.percent >= config.enter_trade)
 
     },
 
     async exit(trader) {
         if (trader.currentTrade?.isBelowStopLoss()) {
+            lossCount++
             await trader.stopTrade()
         } else if (trader.currentTrade?.isPumping()) {
             //return
         } else if (trader.currentTrade?.isAboveTakeProfit()) {
+            gotProfit = true
             await trader.stopTrade()
         }/* else if (currentTrade?.IsBelowEnterTrade()) {
                     consola.info('Stop trade')
