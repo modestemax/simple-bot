dad6477c014a1dbf639cda01007a366590724b1f
modestemax <modestemax@gmail.com>
2020-12-30 18:37:17 +0100

    ....

--- src/strategies/_first_.mjs
+++ src/strategies/_first_.mjs
@@ -1,6 +1,8 @@
 import {cryptoMap} from "../db/index.mjs";
 import firestore from "../db/firestore.mjs";
 import consola from "consola";
+import sendgrid from "../email";
+import {config} from "../db/firestore";
 
 let gotProfit
 let lossCount = {}
@@ -16,18 +18,22 @@ export default {
     enter(signal) {
         const sortedByPercent = Object.values(cryptoMap).filter(a => a.percent)
             .sort((a, b) => a.percent < b.percent ? 1 : -1)
-        return !gotProfit && (lossCount[signal.symbol] || 0) < config.symbol_max_loss && (signal.symbol === sortedByPercent[0]?.symbol && signal.isAboveEnterTrade() && signal.isNotPick())
-
+        const enter = !gotProfit && (lossCount[signal.symbol] || 0) < config.symbol_max_loss && (signal.symbol === sortedByPercent[0]?.symbol && signal.isAboveEnterTrade() && signal.isNotPick())
+        if (enter) {
+            sendgrid.send({body: `enter trade:\nsignal=${JSON.stringify(signal)}\nconfig=${JSON.stringify(config)}`})
+        }
+        return enter
     },
 
     async exit(trader) {
         if (trader.currentTrade?.isBelowStopLoss() && trader.currentTrade?.isNotPick()) {
             try {
                 await trader.stopTrade()
+                sendgrid.send({body: `out trade:\ntrade=${JSON.stringify(trader)}\nconfig=${JSON.stringify(config)}`})
             } finally {
                 lossCount[trader.currentTrade.symbol] = (lossCount[trader.currentTrade.symbol] || 0) + 1
                 await firestore.saveLossCount(lossCount)
-                sendgrid.send({subject, body: `Bad trade for ${trader.currentTrade.symbol}`})
+                sendgrid.send({subject, body: `Bad trade for ${trader.currentTrade.symbol}\nlossCount=${JSON.stringify(lossCount)}`})
             }
 
         } else if (trader.currentTrade?.isPumping()) {
